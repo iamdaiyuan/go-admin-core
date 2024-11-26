d084e5920af13ab3f44cfd8fe9ff3ba9b83df9fd
iamdaiyuan <iamdaiyuan@gmail.com>
2024-11-26 09:49:42 +0800

    Update ip.go

--- sdk/pkg/ip.go
+++ sdk/pkg/ip.go
@@ -10,30 +10,31 @@ import (
 
 // 获取外网ip地址
 func GetLocation(ip, key string) string {
-	if ip == "127.0.0.1" || ip == "localhost" {
-		return "内部IP"
-	}
-	url := "https://restapi.amap.com/v5/ip?ip=" + ip + "&type=4&key=" + key
-	fmt.Println("url", url)
-	resp, err := http.Get(url)
-	if err != nil {
-		fmt.Println("restapi.amap.com failed:", err)
-		return "未知位置"
-	}
-	defer resp.Body.Close()
-	s, err := ioutil.ReadAll(resp.Body)
-	fmt.Println(string(s))
+	return "内部IP"
+	// if ip == "127.0.0.1" || ip == "localhost" {
+	// 	return "内部IP"
+	// }
+	// url := "https://restapi.amap.com/v5/ip?ip=" + ip + "&type=4&key=" + key
+	// fmt.Println("url", url)
+	// resp, err := http.Get(url)
+	// if err != nil {
+	// 	fmt.Println("restapi.amap.com failed:", err)
+	// 	return "未知位置"
+	// }
+	// defer resp.Body.Close()
+	// s, err := ioutil.ReadAll(resp.Body)
+	// fmt.Println(string(s))
 
-	m := make(map[string]string)
+	// m := make(map[string]string)
 
-	err = json.Unmarshal(s, &m)
-	if err != nil {
-		fmt.Println("Umarshal failed:", err)
-	}
-	//if m["province"] == "" {
-	//	return "未知位置"
-	//}
-	return m["country"] + "-" + m["province"] + "-" + m["city"] + "-" + m["district"] + "-" + m["isp"]
+	// err = json.Unmarshal(s, &m)
+	// if err != nil {
+	// 	fmt.Println("Umarshal failed:", err)
+	// }
+	// //if m["province"] == "" {
+	// //	return "未知位置"
+	// //}
+	// return m["country"] + "-" + m["province"] + "-" + m["city"] + "-" + m["district"] + "-" + m["isp"]
 }
 
 // 获取局域网ip地址
